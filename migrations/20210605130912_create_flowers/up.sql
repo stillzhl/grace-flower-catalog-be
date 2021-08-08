6dd67665e16518f6822a137e318c7cfe7c304d64
Haili Zhang <hailizhang@appannie.com>
2021-08-08 13:34:19 -0700

    First workable commit

---
+++ migrations/20210605130912_create_flowers/up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE flowers (
+    flw_id VARCHAR NOT NULL PRIMARY KEY,
+    flw_source VARCHAR NOT NULL,
+    flw_name VARCHAR NOT NULL,
+    flw_img VARCHAR NOT NULL,
+    flw_family VARCHAR NOT NULL,
+    flw_season VARCHAR NOT NULL,
+    flw_desc VARCHAR NOT NULL,
+    flw_site_chars VARCHAR NOT NULL,
+    flw_plant_traits VARCHAR NOT NULL,
+    flw_special_cons VARCHAR NOT NULL,
+    flw_growing_infos VARCHAR NOT NULL,
+    flw_varieties VARCHAR NOT NULL
+);
